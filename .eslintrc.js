4df6ad372dadc88adf3ca7f3b1749e2e136f4961
AlekseyLeshko <alekseyleshko@gmail.com>
2021-01-31 17:24:15 +0400

    Add the eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  rules: {
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        packageDir: [
+          __dirname,
+        ],
+      },
+    ],
+  },
+};
